9711af9401a7419b460e431fa3432f173948227f
Shaurya Jha <shauryajha35@gmail.com>
2024-11-24 21:36:24 +0530

    Add client

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen bg-black text-white flex flex-col'>
+        hi there
+      </div>
+    </>
+  )
+}
+
+export default App
