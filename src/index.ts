9de9a27a8384ea5b11776e25e6f98f4fedccf1f0
Shaurya Jha <shauryajha35@gmail.com>
2024-11-24 21:13:08 +0530

    Add join and chat socket

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,32 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+// let allSockets = {};
+
+interface User{
+    socket: WebSocket;
+    room: String;
+}
+
+let allSockets: User[] = [];
+
 
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-
-    userCount = userCount + 1;
-    console.log("user connected #"+ userCount);
     
     socket.on("message", (message) => {
-        console.log("Received message " +message.toString());
-
-        // single to single message connection
-        // socket.send(message.toString()+ " : sent from the server");
-
-        // broadcast to all connected clients
-        for(let i = 0; i< allSockets.length; i++){
-            const s = allSockets[i];
-            s.send(message.toString()+ ": sent from the server");
+        const parseMessage = JSON.parse(message);
+        if(parseMessage.type == "join"){
+            allSockets.push({
+                socket,
+                room: parseMessage.payload.roomId
+            })
         }
+        
     })
 
     socket.on("disconnect", () => {
         allSockets = allSockets.filter(x => x != socket);
+
     })
 })
 
