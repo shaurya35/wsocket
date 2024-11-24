df28d3c382b9345b158d23782c82f725fa833b66
Shaurya Jha <shauryajha35@gmail.com>
2024-11-25 02:39:55 +0530

    Bug: Fix types client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,32 @@
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const ws = new WebSocket("https://localhost:8080/");
+    ws.onmessage = (event) => {
+      setMessages(m => [...m, event.data]);
+    }
+  }, []);
 
   return (
     <>
-      <div className='h-screen bg-black text-white flex flex-col'>
-        hi there
+      <div className="h-screen bg-black text-white flex flex-col">
+        <br />
+        <div className="h-[95vh] bg-gray-500">
+          {messages.map((message) => (
+            <span>{message}</span>
+          ))}
+        </div>
+        <div className="w-full bg-white flex">
+          <input type="text" className="flex-1 p-4" />
+          <button className="bg-purple-600 text-white p-4" onClick={() => {}}>Send</button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
