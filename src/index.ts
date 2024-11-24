63b809180cc23de07ae9e8e2ae346b28526f779f
Shaurya Jha <shauryajha35@gmail.com>
2024-11-24 21:21:22 +0530

    Add que

--- src/index.ts
+++ src/index.ts
@@ -15,19 +15,31 @@ let allSockets: User[] = [];
 wss.on("connection", (socket) => {
     
     socket.on("message", (message) => {
-        const parseMessage = JSON.parse(message);
+        const parseMessage = JSON.parse(message as unknown as string);
+
+        // join the chatroom
         if(parseMessage.type == "join"){
             allSockets.push({
                 socket,
                 room: parseMessage.payload.roomId
             })
         }
+
+        // if chat, checks room then send the message
+        if(parseMessage.type == "chat"){
+            let currentUserRoom = null;
+            for(let i = 0; i < allSockets.length; i++){
+                if(allSockets[i].socket == socket){
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+            for(let i = 0; i < allSockets.length; i++ ){
+                if(allSockets[i].room == currentUserRoom){
+                    allSockets[i].socket.send(parseMessage.payload.message)
+                }
+            }
+        }
         
-    })
-
-    socket.on("disconnect", () => {
-        allSockets = allSockets.filter(x => x != socket);
-
     })
 })
 
